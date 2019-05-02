9f783d4e0a7f55c072f87f7df4ae38967350e590
raedalkhaldi <raedalkhaldi@gmail.com>
2019-05-02 09:21:44 +0300

    second commit

--- main.js
+++ main.js
@@ -7,16 +7,16 @@ $(document).ready(function () {
     var notLikedMovies = [];
     var notWatchedMovies = [];
 
-
+// to create more forms view
     for (var i = 0; i < 9; i++) {
         allForms.push(forms.clone())
         $(allForms[i]).attr('id', i + 1);
 
     }
     
-
+// append forms
     $('body').append(allForms);
-    var randomIndex = Math.floor(Math.random() * 807) + 1;
+    // get movies data from API
     $.ajax({
         method: 'GET',
         url: 'https://api.themoviedb.org/3/movie/top_rated?api_key=cabb891b02522fd385fe4b50cf6995eb',
@@ -26,6 +26,7 @@ $(document).ready(function () {
         }
 
     })
+    // function to render movies data
     function renerApiData(data) {
 
         for (var i = 0; i < 10; i++) {
@@ -42,7 +43,7 @@ $(document).ready(function () {
     }
 
     $().on('click')
-
+// like button
     $('button[type="like"]').one("click", (function (event) {
         event.preventDefault();
         console.log($(`#${$('button[type="like"]').index(this)} > button[type="like"]`));
@@ -54,7 +55,7 @@ $(document).ready(function () {
 
 
     }))
-
+// not like 
     $('button[type="no_like"]').one("click", (function (event) {
         event.preventDefault();
         console.log("you did it");
@@ -65,7 +66,7 @@ $(document).ready(function () {
 
 
     }))
-
+// not watched
     $('button[type="no_see"]').one("click", (function (event) {
         event.preventDefault();
         console.log("you did it");
@@ -77,6 +78,8 @@ $(document).ready(function () {
 
 
     }))
+
+    // ready button
     $('button[type="ready"]').one("click", (function (event) {
      
         var notWatchedObjects = notWatchedMovies.map(function (movie){
@@ -105,22 +108,13 @@ $(document).ready(function () {
         $('h2').text(notWatchedObjects[rand].title);
         $('button[type="ready"]').empty();
         $('button[type="ready"]').remove();
-
         $('#rate').text(" Vote Average = " + notWatchedObjects[rand].vote_average);
         $('img').attr('src', 'http://image.tmdb.org/t/p/w185/' + notWatchedObjects[rand].poster_path);
-        $('body').append('<iframe width="560" height="345" src="https://www.imdb.com/videoembed/vi2163260441"> </iframe>');
-        // $('body').css("background-color", "#383838");
-        // $('p').css("color", "white");
-
-    
+        $('body').append('<iframe width="560" height="345" src="https://www.imdb.com/videoembed/vi2163260441"> </iframe>');  
         $('#refresh').one("click", (function (event) {
-            
             location.reload();
         }))
-function autoPlayVideo(vcode, width, height){
-  "use strict";
-  $("#videoContainer").html('<iframe width="'+width+'" height="'+height+'" src="https://www.youtube.com/embed/'+vcode+'?autoplay=1&loop=1&rel=0&wmode=transparent" frameborder="0" allowfullscreen wmode="Opaque"></iframe>');
-}
+
 
     })
 
